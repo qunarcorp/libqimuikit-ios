8e6cab3dd38f7dcfe0bd95248ef7c7cb32e20765
lilu <lilu@liludeMacBook-Pro.local>
2019-02-13 21:05:18 +0800

    QIMWebView适配iPad & 修复通知消息自适应无法高度

--- QIMUIKit/QTalkMessageBaloon/QIMChatNotifyInfoCell.m
+++ QIMUIKit/QTalkMessageBaloon/QIMChatNotifyInfoCell.m
@@ -34,11 +34,11 @@ static double _global_message_cell_width = 0;
     
     UILabel *label = [[UILabel alloc] init];
     label.text = message.message;
-    label.width = kCellWidth;
+    label.width = kCellWidth * 4/5;
     label.numberOfLines = 0;
     label.font = [UIFont systemFontOfSize:12];
     [label sizeToFit];
-    return label.height+ 20;
+    return label.height + 20;
     //去特么的第三方，算的高度是错的
     return [MDHTMLLabel sizeThatFitsHTMLString:message.message withFont:[UIFont systemFontOfSize:12] constraints:CGSizeZero limitedToNumberOfLines:0] + 20;
 }
