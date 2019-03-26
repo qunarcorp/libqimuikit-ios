eb1236073bed75f5a59874019eb03a3565d4a1d2
lilu <lilu@liludeMacBook-Pro.local>
2019-03-26 17:24:08 +0800

    update

---
+++ QIMUIKit/Application/ViewController/Nav/QIMNacConfigTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  QIMNacConfigTableViewCell.h
+//  QIMUIKit
+//
+//  Created by lilu on 2019/3/26.
+//  Copyright © 2019 QIM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QIMNacConfigTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ QIMUIKit/Application/ViewController/Nav/QIMNacConfigTableViewCell.m
@@ -0,0 +1,36 @@
+//
+//  QIMNacConfigTableViewCell.m
+//  QIMUIKit
+//
+//  Created by lilu on 2019/3/26.
+//  Copyright © 2019 QIM. All rights reserved.
+//
+
+#import "QIMNacConfigTableViewCell.h"
+
+@interface QIMNacConfigTableViewCell ()
+
+@end
+
+@implementation QIMNacConfigTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
