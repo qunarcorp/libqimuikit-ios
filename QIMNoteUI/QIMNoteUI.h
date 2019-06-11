4ddf0e5c8dcd1e8d188bb863cb8697c4607b5fea
lilu <lilu@liludeMacBook-Pro.local>
2019-06-11 18:08:40 +0800

    update

--- QIMNoteUI/QIMNoteUI.h
+++ QIMNoteUI/QIMNoteUI.h
@@ -6,7 +6,7 @@
 //  Copyright © 2018年 QIM. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <UIKit/UIKit.h> 
 
 //! Project version number for QIMNoteUI.
 FOUNDATION_EXPORT double QIMNoteUIVersionNumber;
