2f852311d9444f906a7492fb8790884e2f7d3fce
lilu <lilu@liludeMacBook-Pro.local>
2019-07-01 22:01:00 +0800

    startalk更新

--- QIMUIVendorKit/QIMUpdateView/QIMUpdateAlertView.m
+++ QIMUIVendorKit/QIMUpdateView/QIMUpdateAlertView.m
@@ -13,19 +13,25 @@
 #import "NSString+QIMUtility.h"
 
 #define key_Window [UIApplication sharedApplication].keyWindow
-#define key_HCSelectItem_Title @"key_HCSelectItem_Title"
-#define key_HCSelectItem_TitleColor @"key_HCSelectItem_TitleColor"
+#define key_QIMSelectItem_Title @"key_QIMSelectItem_Title"
+#define key_QIMSelectItem_TitleColor @"key_QIMSelectItem_TitleColor"
 
 #define kColorWithHex(c) [UIColor colorWithRed:((c>>16)&0xFF)/255.0f green:((c>>8)&0xFF)/255.0f blue:(c&0xFF)/255.0f alpha:1.0f]
 
-#define Screen_HC_Width [UIScreen mainScreen].bounds.size.width
-#define Screen_HC_Height [UIScreen mainScreen].bounds.size.height
+#define Screen_QIM_Width [UIScreen mainScreen].bounds.size.width
+#define Screen_QIM_Height [UIScreen mainScreen].bounds.size.height
 #define kAlertView_offsetX 55   //alertView侧边偏移量
 #define kAlertView_MAX_Height 460   //alertView最大高度
 #define kAlertView_content_offsetX 20 //内部文字边距
 #define kSelect_item_height 45 //选项高度
 
 @interface QIMUpdateAlertView ()
+
+@property(nonatomic, assign) QIMUpgradeType upgradeType;    //更新状态
+
+@property(nonatomic, copy) NSString *updateUrl;     //更新地址
+
+@property(nonatomic, strong) NSDictionary *updateDic;   //更新Dic
 
 @property(nonatomic, copy) NSString *title;
 
@@ -71,8 +77,10 @@
         _messageLabel = [[UILabel alloc] initWithFrame:CGRectZero];
         _messageLabel.text = @"更新内容更新内容更新内容更新内容更新内容更新内容";
         _messageLabel.font = [UIFont systemFontOfSize:14];
-        _messageLabel.numberOfLines = 0;
+        _messageLabel.numberOfLines = 5;
         _messageLabel.textColor = [UIColor qim_colorWithHex:0x888888];
+        _messageLabel.backgroundColor = [UIColor whiteColor];
+        _messageLabel.lineBreakMode = NSLineBreakByTruncatingTail;
     }
     return _messageLabel;
 }
@@ -87,6 +95,7 @@
         [_nextButton setTitle:@"下次再说" forState:UIControlStateNormal];
         [_nextButton setTitleColor:[UIColor qim_colorWithHex:0x999999] forState:UIControlStateNormal];
         [_nextButton setBackgroundColor:[UIColor whiteColor]];
+        [_nextButton addTarget:self action:@selector(nextUpdate:) forControlEvents:UIControlEventTouchUpInside];
     }
     return _nextButton;
 }
@@ -99,47 +108,49 @@
         [_updateButton setTitle:@"立即更新" forState:UIControlStateNormal];
         [_updateButton setTitleColor:[UIColor qim_colorWithHex:0xFFFFFF] forState:UIControlStateNormal];
         [_updateButton setBackgroundColor:[UIColor qim_colorWithHex:0x00CABE]];
+        [_updateButton addTarget:self action:@selector(gotoUpdate:) forControlEvents:UIControlEventTouchUpInside];
     }
     return _updateButton;
 }
 
 - (UIView *)alertView {
     if (!_alertView) {
-        _alertView = [[UIView alloc] initWithFrame:CGRectMake(kAlertView_offsetX, 0, Screen_HC_Width - kAlertView_offsetX * 2, kAlertView_MAX_Height / 2)];
+        _alertView = [[UIView alloc] initWithFrame:CGRectMake(kAlertView_offsetX, 0, Screen_QIM_Width - kAlertView_offsetX * 2, kAlertView_MAX_Height / 2)];
         _alertView.center = key_Window.center;
         _alertView.backgroundColor = [UIColor whiteColor];
         _alertView.layer.masksToBounds = YES;
-        _alertView.layer.cornerRadius = 5;
+        _alertView.layer.cornerRadius = 3.0f;
     }
     return _alertView;
 }
 
-+ (void)qim_showUpdateAlertViewTitle:(NSString *)title message:(NSString *)message selectItemIndex:(NSInteger)index {
-    QIMUpdateAlertView *updateView = [[QIMUpdateAlertView alloc] initWithFrame:CGRectZero];
-    [updateView showAlertViewTitle:title message:message selectItemIndex:index];
++ (instancetype)qim_showUpdateAlertViewWithUpdateDic:(NSDictionary *)updateDic {
+    QIMUpdateAlertView *updateView = [[QIMUpdateAlertView alloc] initWithFrame:[[UIApplication sharedApplication] keyWindow].bounds];
+
+    [updateView showUpdateAlertViewWithUpdateDic:updateDic];
+    return updateView;
 }
 
-- (void)showAlertViewTitle:(NSString *)title message:(NSString *)message selectItemIndex:(NSInteger)index {
-//    self.selectedBlock = selectIndex;
-//    itemsBlock(self.items);
+- (void)showUpdateAlertViewWithUpdateDic:(NSDictionary *)updateDic {
 
-    self.title = title;
-    self.message = message;
-
+    NSString *updateTitle = @"新版本更新";
+    NSString *updateMessage = [updateDic objectForKey:@"copywriting"];
+    self.upgradeType = [[updateDic objectForKey:@"upgradeStatus"] integerValue];
+    self.updateDic = updateDic;
+    self.title = updateTitle;
+    self.message = updateMessage;
+    self.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.35];
     [[[UIApplication sharedApplication] keyWindow] addSubview:self];
 
     [self addSubview:self.alertView];
     [self layoutHCAlertView];
-    self.alertView.center = key_Window.center;
 }
-
 
 /**
  布局AlertView
  */
 - (void)layoutHCAlertView {
 
-    self.alertView.backgroundColor = [UIColor yellowColor];
     [self.alertView addSubview:self.iconView];
     [self.iconView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.mas_offset(1);
@@ -161,62 +172,85 @@
     self.titleLabel.text = self.title;
 
     //messageLabel
-    CGFloat normalMessageWidth = self.alertView.frame.size.width - kAlertView_content_offsetX * 2;
-    CGSize rectMessageSize = [self.message qim_sizeWithFontCompatible:self.messageLabel.font forWidth:normalMessageWidth lineBreakMode:NSLineBreakByWordWrapping];
-
-
+    CGFloat normalMessageWidth = self.alertView.frame.size.width - kAlertView_content_offsetX * 2 - 56;
+    CGSize rectMessageSize = [self.message qim_sizeWithFontCompatible:self.messageLabel.font constrainedToSize:CGSizeMake(normalMessageWidth, MAXFLOAT) lineBreakMode:NSLineBreakByTruncatingTail];
     self.messageLabel.text = self.message;
     [self.alertView addSubview:self.messageLabel];
     [self.messageLabel mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.mas_equalTo(self.titleLabel.mas_bottom).mas_offset(14);
-        make.width.mas_equalTo(rectMessageSize.width);
-        make.height.mas_equalTo(rectMessageSize.height);
+        make.left.mas_equalTo(28);
+        make.right.mas_equalTo(-28);
+        make.height.mas_equalTo((rectMessageSize.height > self.messageLabel.font.lineHeight * 6) ? self.messageLabel.font.lineHeight * 6 : rectMessageSize.height);
         make.centerX.mas_equalTo(self.alertView.mas_centerX);
     }];
 
-    //Button
-    [self.alertView addSubview:self.nextButton];
-    [self.nextButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.width.mas_equalTo(105);
-        make.height.mas_equalTo(42);
-        make.left.mas_offset(20);
-        make.top.mas_equalTo(self.messageLabel.mas_bottom).mas_offset(26);
-    }];
+    if (self.upgradeType == QIMUpgradeAfter) {
+        //下次再说按钮
+        [self.alertView addSubview:self.nextButton];
+        [self.nextButton mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.width.mas_equalTo(105);
+            make.height.mas_equalTo(42);
+            make.left.mas_offset(20);
+            make.top.mas_equalTo(self.messageLabel.mas_bottom).mas_offset(26);
+        }];
+        
+        //立即更新按钮
+        [self.alertView addSubview:self.updateButton];
+        [self.updateButton mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.width.mas_equalTo(105);
+            make.height.mas_equalTo(42);
+            make.right.mas_equalTo(-20);
+            make.top.mas_equalTo(self.messageLabel.mas_bottom).mas_offset(26);
+        }];
+    } else {
+        //立即更新按钮
+        [self.alertView addSubview:self.updateButton];
+        [self.updateButton mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.width.mas_equalTo(210);
+            make.height.mas_equalTo(42);
+            make.right.mas_equalTo(-28);
+            make.top.mas_equalTo(self.messageLabel.mas_bottom).mas_offset(26);
+        }];
+    }
 
-    [self.alertView addSubview:self.updateButton];
-    [self.updateButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.width.mas_equalTo(105);
-        make.height.mas_equalTo(42);
-        make.right.mas_equalTo(-20);
-        make.top.mas_equalTo(self.messageLabel.mas_bottom).mas_offset(26);
-    }];
-
-    //alertView frame
-//    CGFloat alertViewHeight = self.titleLabel.frame.origin.y + rectTitleSize.height + rectMessageSize.height + 100 + kAlertView_content_offsetX * 2;
-    self.alertView.frame = CGRectMake(kAlertView_offsetX, 0, Screen_HC_Width - kAlertView_offsetX * 2, self.updateButton.bottom + 60);
-//    self.alertView.frame = CGRectMake(kAlertView_offsetX, 0, Screen_HC_Width - kAlertView_offsetX * 2, alertViewHeight > kAlertView_MAX_Height ? kAlertView_MAX_Height : alertViewHeight);
-    self.alertView.center = key_Window.center;
-//    _alertView = [[UIView alloc] initWithFrame:CGRectMake(kAlertView_offsetX, 0, Screen_HC_Width - kAlertView_offsetX * 2, kAlertView_MAX_Height / 2)];
 
     [self.alertView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.width.mas_equalTo(Screen_HC_Width - kAlertView_offsetX * 2);
+        make.width.mas_equalTo(Screen_QIM_Width - kAlertView_offsetX * 2);
         make.bottom.mas_equalTo(self.updateButton.mas_bottom).mas_offset(30);
         make.left.mas_equalTo(kAlertView_offsetX);
-        make.top.mas_equalTo(Screen_HC_Width - kAlertView_offsetX * 2);
-//        make.center.mas_equalTo([UIApplication sharedApplication].keyWindow.mas_center);
+        make.top.mas_equalTo(Screen_QIM_Width - kAlertView_offsetX * 2);
     }];
 
-//    NSMutableArray *arr = self.items.itemsArray;
-//    for (NSDictionary *hcItem in arr) {
-//        NSLog(@"items: %@", [hcItem objectForKey:key_HCSelectItem_Title]);
-//    }
-//npm
-//    self.selectedBlock(2);
+    [self layoutIfNeeded];
+    self.alertView.center = key_Window.center;
 }
 
-- (void)selectActionWhenTwoItem:(UIButton *)sender {
-//    self.selectedBlock(sender.tag);
-//    [self hiddenSelf];
+#pragma mark - Action
+
+- (void)dismiss
+{
+    [UIView animateWithDuration:0.3f animations:^{
+        self.alpha = 0;
+    } completion:^(BOOL finished) {
+        [self removeFromSuperview];
+    }];
+    
+}
+
+- (void)nextUpdate:(id)sender {
+    //下次再说
+    if (self.upgradeDelegate && [self.upgradeDelegate respondsToSelector:@selector(qim_UpdateAlertViewDidClickWithType:withUpdateDic:)]) {
+        [self.upgradeDelegate qim_UpdateAlertViewDidClickWithType:QIMUpgradeAfter withUpdateDic:self.updateDic];
+    }
+    [self dismiss];
+}
+
+- (void)gotoUpdate:(id)sender {
+    //现在去更新
+    if (self.upgradeDelegate && [self.upgradeDelegate respondsToSelector:@selector(qim_UpdateAlertViewDidClickWithType:withUpdateDic:)]) {
+        [self.upgradeDelegate qim_UpdateAlertViewDidClickWithType:QIMUpgradeNow withUpdateDic:self.updateDic];
+    }
+    [self dismiss];
 }
 
 @end
