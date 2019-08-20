3b32e7aba132024b67f47f5e7ab8f7c0d821356a
lilu <lilulucas.li@qunar.com>
2019-08-20 14:29:22 +0800

    qchat订单提醒消息ui改版 & 封装imageview，image，解决gif内存飙的问题

---
+++ QIMUIKit/General/Helper/QIMImage/QIMImageView.m
@@ -0,0 +1,20 @@
+//
+//  QIMImageView.m
+//  QIMUIKit
+//
+//  Created by lilu on 2019/8/20.
+//
+
+#import "QIMImageView.h"
+
+@implementation QIMImageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
